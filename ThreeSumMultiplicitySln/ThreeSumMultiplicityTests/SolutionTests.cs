45b5d4eca8ab3b38436a5177f192b943cbd5fd89
dionK <dion.kllokoqi@gmail.com>
2022-04-19 16:25:29 +0200

    Add simple test and update generic one

--- ThreeSumMultiplicitySln/ThreeSumMultiplicityTests/SolutionTests.cs
+++ ThreeSumMultiplicitySln/ThreeSumMultiplicityTests/SolutionTests.cs
@@ -6,16 +6,31 @@ namespace ThreeSumMultiplicityTests;
 public class SolutionTests
 {
     [Fact]
-    public void ThreeSumMulti_OnGenericInput_ReturnsNrOfTuples()
+    public void ThreeSumMulti_OnSimpleInput_ReturnsNrOfTuples()
     {
         // Arrange
         var threeTierMulti = new Solution();
-        int[] arr = new int[] {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
-        int target = 8;
+        int[] arr = new int[] {2, 1, 3};
+        int target = 6;
 
         // Act
         var actual = threeTierMulti.ThreeSumMulti(arr, target);
-        var expected = 20;
+        var expected = 1;
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(new int[] {1, 1, 2, 2, 2, 2}, 5, 12)]
+    [InlineData(new int[] {1, 1, 2, 2, 3, 3, 4, 4, 5, 5}, 8, 20)]
+    public void ThreeSumMulti_OnGenericInput_ReturnsNrOfTuples(int[] arr, int target, int expected)
+    {
+        // Arrange
+        var threeTierMulti = new Solution();
+
+        // Act
+        var actual = threeTierMulti.ThreeSumMulti(arr, target);
 
         // Assert
         Assert.Equal(expected, actual);
