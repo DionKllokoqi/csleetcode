9a5df6191fca22e354c0a001bc0e8039d0309400
dion <dion.kllokoqi@gmail.com>
2022-04-21 17:12:08 +0200

    Add working solution with squared complexity.

--- TwoSumSln/TwoSum/TwoSumSolution.cs
+++ TwoSumSln/TwoSum/TwoSumSolution.cs
@@ -4,6 +4,17 @@ public class TwoSumSolution
 {
     public int[] TwoSum(int[] arr, int target)
     {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if ((arr[i] + arr[j]) == target)
+                {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
         return new int[0];
     }
 }
--- TwoSumSln/TwoSumTests/TwoSumSolutionTests.cs
+++ TwoSumSln/TwoSumTests/TwoSumSolutionTests.cs
@@ -6,7 +6,9 @@ namespace TwoSumTests;
 public class TwoSumSolutionTests
 {
     [Theory]
-    [InlineData(new int[]{2, 7, 11, 15}, 9, new int[]{0, 1})]
+    [InlineData(new int[] {2, 7, 11, 15}, 9, new int[] {0, 1})]
+    [InlineData(new int[] {3, 2, 4}, 6, new int[] {1, 2})]
+    [InlineData(new int[] {3, 3}, 6, new int[] {0, 1})]
     public void TwoSum_ShouldReturnTupleOfIndicesThatAddUpToTarget(int[] arr, int target, int[] expected)
     {
         // Arrange
