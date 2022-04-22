b23a5d4fe0720bab37bc89c110f281daa152d15f
dionK <dion.kllokoqi@gmail.com>
2022-04-22 11:17:08 +0200

    Add efficient algorithm.

--- TwoSumSln/TwoSum/TwoSumSolution.cs
+++ TwoSumSln/TwoSum/TwoSumSolution.cs
@@ -17,4 +17,44 @@ public class TwoSumSolution
 
         return new int[0];
     }
+
+    public int[] TwoSumEfficient(int[] arr, int target)
+    {
+        var sortedArr = new int[arr.Length];
+        arr.CopyTo(sortedArr, 0);
+        Array.Sort(sortedArr);
+
+        int i = 0;
+        int j = arr.Length - 1;
+
+        while (true)
+        {
+            if ((sortedArr[i] + sortedArr[j]) == target)
+            {
+                if (sortedArr[i] != sortedArr[j])
+                {
+                    i = Array.IndexOf(arr, sortedArr[i]);
+                    j = Array.IndexOf(arr, sortedArr[j]);
+                }
+                else
+                {
+                    i = Array.IndexOf(arr, sortedArr[i]);
+                    j = Array.IndexOf(arr, sortedArr[j], i+1);
+                }
+                return new int[] {i, j};
+            }
+            else if ((sortedArr[i] + sortedArr[j]) > target)
+            {
+                j--;
+            }
+            else if ((sortedArr[i] + sortedArr[j]) < target)
+            {
+                i++;
+            }
+            else
+            {
+                return new int[0];
+            }
+        }
+    }
 }
--- TwoSumSln/TwoSumTests/TwoSumSolutionTests.cs
+++ TwoSumSln/TwoSumTests/TwoSumSolutionTests.cs
@@ -20,4 +20,21 @@ public class TwoSumSolutionTests
         // Assert
         Assert.Equal(expected, actual);
     }
+
+    
+    [Theory]
+    [InlineData(new int[] {2, 7, 11, 15}, 9, new int[] {0, 1})]
+    [InlineData(new int[] {3, 2, 4}, 6, new int[] {1, 2})]
+    [InlineData(new int[] {3, 3}, 6, new int[] {0, 1})]
+    public void TestTwoSumEfficient_ShouldReturnTupleOfIndicesThatAddUpToTarget(int[] arr, int target, int[] expected)
+    {
+        // Arrange
+    	var twoSumSolution = new TwoSumSolution();
+
+        // Act
+        var actual = twoSumSolution.TwoSumEfficient(arr, target);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
 }
