ce39906c8b8ec0c60f42090cbe18af0e0b846f85
dionK <dion.kllokoqi@gmail.com>
2022-04-23 15:12:47 +0200

    Add O(n) solution via Hash Table

--- TwoSumSln/TwoSum/TwoSumSolution.cs
+++ TwoSumSln/TwoSum/TwoSumSolution.cs
@@ -31,7 +31,7 @@ public class TwoSumSolution
     /// <param name="arr">Input array.</param>
     /// <param name="target">Target value the sum of two arr elements should add up to.</param>
     /// <returns>Array with two elements representing the indices of the solution, or empty array.</returns>
-    public int[] TwoSumEfficient(int[] arr, int target)
+    public int[] TwoSumNlogN(int[] arr, int target)
     {
         var sortedArr = new int[arr.Length];
         arr.CopyTo(sortedArr, 0);
@@ -72,5 +72,30 @@ public class TwoSumSolution
                 return new int[0];
             }
         }
+    }
+
+    /// <summary>
+    /// Method solves the problem in O(n). arr values are added as keys to Dictionary / Hash Table,
+    /// whereas their index is the value. We are simultaneously adding elements to the dictionary,
+    /// while checking if the difference target - arr[i] already exists as a key in the dictionary.
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public int[] ToSumNHash(int[] arr, int target)
+    {
+        Dictionary<int, int> ht = new Dictionary<int, int>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var complement = target - arr[i];
+            if (ht.ContainsKey(complement))
+            {
+                return new int[] { ht[complement], i };
+            }
+            ht[arr[i]] = i;
+        }
+
+        return new int[0];
     }
 }
--- TwoSumSln/TwoSumTests/TwoSumSolutionTests.cs
+++ TwoSumSln/TwoSumTests/TwoSumSolutionTests.cs
@@ -26,13 +26,29 @@ public class TwoSumSolutionTests
     [InlineData(new int[] {2, 7, 11, 15}, 9, new int[] {0, 1})]
     [InlineData(new int[] {3, 2, 4}, 6, new int[] {1, 2})]
     [InlineData(new int[] {3, 3}, 6, new int[] {0, 1})]
-    public void TestTwoSumEfficient_ShouldReturnTupleOfIndicesThatAddUpToTarget(int[] arr, int target, int[] expected)
+    public void TestTwoSumNlogN_ShouldReturnTupleOfIndicesThatAddUpToTarget(int[] arr, int target, int[] expected)
     {
         // Arrange
     	var twoSumSolution = new TwoSumSolution();
 
         // Act
-        var actual = twoSumSolution.TwoSumEfficient(arr, target);
+        var actual = twoSumSolution.TwoSumNlogN(arr, target);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(new int[] {2, 7, 11, 15}, 9, new int[] {0, 1})]
+    [InlineData(new int[] {3, 2, 4}, 6, new int[] {1, 2})]
+    [InlineData(new int[] {3, 3}, 6, new int[] {0, 1})]
+    public void TestTwoSumNHash_ShouldReturnTupleOfIndicesThatAddUpToTarget(int[] arr, int target, int[] expected)
+    {
+        // Arrange
+        var twoSumSolution = new TwoSumSolution();
+
+        // Act
+        var actual = twoSumSolution.ToSumNHash(arr, target);
 
         // Assert
         Assert.Equal(expected, actual);
